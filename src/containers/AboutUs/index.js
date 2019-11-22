8837c73b51d0d23fc31fa46a71f37ada11d95c9c
eidininja <lucaseidikumagai@gmail.com>
2019-11-22 14:21:13 -0300

    ajeitando o about

--- src/containers/AboutUs/index.js
+++ src/containers/AboutUs/index.js
@@ -13,7 +13,11 @@ class About extends Component {
                 <div class="card-header">About Us</div>
                     <div class="card-body text-dark">
                         <h5 class="card-title">Triangulo </h5>
-                        <p class="card-text">A TRIÂNGULO® PISOS E PAINÉIS LTDA. é dedicada à fabricação de produtos de madeira desde 1972. Construída na época, uma sólida estrutura industrial, administrativa e patrimonial que garantiu a evolução tecnológica e constante inovação em sua linha de produtos, adaptando-se às exigências do mercado interno e externo.</p>
+                        <p class="card-text">
+
+                        TRIÂNGULO HARWOOD FLOORING dedicated to the manufacturing of wood products since 1972. Built at that time, a solid industrial structure, administrative and asset that secured the technological evolution and constant innovation in its product line, adapting to the demands of the domestic and foreign markets.
+The knowledge of the peculiarities of the wood, the most important building material and coating throughout the history of humanity and respect for the forest reserves, natural and planted, defined the guidelines of the business activity of TRIÂNGULO: “Man and Nature in Harmony”.               
+                        </p>
                     
                     </div>
                 </div>
@@ -21,11 +25,16 @@ class About extends Component {
                 <div class="card bg-light mb-3" style={{maxWidth:'18rem'}}>
                     <div class="card-header">Contact</div>
                     <div class="card-body">
-                        <h5 class="card-title">TRIÂNGULO PISOS E PAINÉIS LTDA (MATRIZ)</h5>
-                        <p class="card-text">Rua Chanceler Osvaldo Aranha, 570 – Bairro Hauer
-81630-160 – Curitiba – Paraná
-CNPJ : 75.059.857/0001-86
-Inscrição Estadual : 10.126.468-83</p>
+                        <h5 class="card-title">TRIÂNGULO HARWOOD FLOORING</h5>
+                        <p class="card-text">Phone : +1 (757) 420 4771
+Fax : +1 (757) 420 4772
+Email : sales@triangulo.com.br
+Office Hours: Monday – Friday
+8:00 AM – 5:00 PM (Eastern time)
+Warehouse Address
+3804 Cook Blvd Suite 15 Chesapeake, VA 23323
+Mailing Address
+870 Greenbrier Cir. Suite 400 Chesapeake, VA 23320</p>
                     </div>
                 </div>
          
