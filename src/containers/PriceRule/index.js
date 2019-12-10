8662d7bb9bb7162c90edd376efc1a454eb18c6fb
eidininja <lucaseidikumagai@gmail.com>
2019-12-10 16:03:16 -0300

    ajustando o search

--- src/containers/PriceRule/index.js
+++ src/containers/PriceRule/index.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
 import React, {Component} from 'react';
 import { notification } from '../../components';
-import { Table, Divider, Tag, Select, Popconfirm  } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { Table, Divider, Tag, Select, Popconfirm, Icon, Input, Button  } from 'antd';
 import api from '../../containers/Page/api';
 const {Option} = Select;
   
@@ -51,6 +52,72 @@ class PriceRule extends Component {
         this.setState({info: resposta.data});
       });
      }
+
+     getColumnSearchProps = dataIndex => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={node => {
+              this.searchInput = node;
+            }}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon="search"
+            size="small"
+            style={{ width: 90, marginRight: 8 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </div>
+      ),
+      filterIcon: filtered => (
+        <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex]
+          .toString()
+          .toLowerCase()
+          .includes(value.toLowerCase()),
+      onFilterDropdownVisibleChange: visible => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select());
+        }
+      },
+      render: text =>
+        this.state.searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        ) : (
+          text
+        ),
+    });
+  
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      this.setState({
+        searchText: selectedKeys[0],
+        searchedColumn: dataIndex,
+      });
+    };
+  
+    handleReset = clearFilters => {
+      clearFilters();
+      this.setState({ searchText: '' });
+    };
 
      fetchRules(){
       api.get("https://api-triangulo.herokuapp.com/showrules").then(res =>{  
@@ -125,7 +192,8 @@ class PriceRule extends Component {
               title: 'Name',
               dataIndex: 'name',
               key: 'name',
-              render: text => <a>{text}</a>,
+              ...this.getColumnSearchProps('name')
+              
             },
             {
               title: 'Price Rule',
